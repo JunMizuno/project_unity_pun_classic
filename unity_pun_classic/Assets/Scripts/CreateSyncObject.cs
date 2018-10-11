0ba6fd95e974fa29ef3c7673e4c4d341442ab6a9
水野淳 <>
2018-10-11 16:16:25 +0900

    PUN classicをインポート、同期テストスクリプトを実装

---
+++ unity_pun_classic/Assets/Scripts/CreateSyncObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSyncObject : MonoBehaviour {
+    private List<GameObject> _cubeList = new List<GameObject>();
+
+    /// <summary>
+    /// 更新時
+    /// </summary>
+	void Update () {
+        float x = Random.Range(-10.0f, 10.0f);
+        float y = Random.Range(-10.0f, 10.0f);
+        float z = Random.Range(-10.0f, 10.0f);
+        Vector3 newPos = new Vector3(x, y, z);
+
+        // 左ボタンクリック時
+        // プレハブ、座標、回転、ビューインデックス
+        if (Input.GetMouseButtonDown(0)) {
+            var cube = PhotonNetwork.Instantiate("Cube", newPos, Quaternion.identity, 0);
+            // 生成したオブジェクトに力を加える
+            Rigidbody objRB = cube.GetComponent<Rigidbody>();
+            objRB.AddForce(Vector3.forward * 20f, ForceMode.Impulse);
+            _cubeList.Add(cube);
+        }
+
+        // 一定距離を離れたら解放する
+        foreach (var cube in _cubeList) {
+            var position = cube.GetComponent<Transform>().position;
+
+            // ループ中にリストからオブジェクトを削除する場合はリストの長さが変わるためループから抜ける
+            if (position.z > 70.0f) {
+                _cubeList.Remove(cube);
+                Destroy(cube);
+                break;
+            }
+        }
+    }
+}
---
+++ unity_pun_classic/Assets/Scripts/PhotonUnityNetworkController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonUnityNetworkController : MonoBehaviour {
+    private string ROOM_NAME = "Room";
+
+    /// <summary>
+    /// 開始時
+    /// </summary>
+	void Start () {
+        // Photonに接続する
+        // @memo. 引数でゲームバージョンを指定できる
+        PhotonNetwork.ConnectUsingSettings(null);	
+	}
+	
+    /// <summary>
+    /// ロビーに入った時
+    /// </summary>
+    void OnJoinedLobby() {
+        Debug.Log("ロビーに入りました");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    /// <summary>
+    /// ルームに入った時
+    /// </summary>
+    void OnJoinedRoom() {
+        Debug.Log("ルームに入りました");
+    }
+
+    /// <summary>
+    /// ルーム入室失敗時
+    /// </summary>
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("ルームの入室に失敗しました。");
+
+        // ルームがないと入室に失敗するため、その時は自分で作る
+        // 引数でルーム名を指定できる
+        PhotonNetwork.CreateRoom(ROOM_NAME);
+    }
+}
